32429200e91379f263848273678984e3fd0e01c4
Emil Söderlind <soderlindemil@gmail.com>
2019-11-14 13:04:07 +0100

    to hex wuuurk

--- src/main.java
+++ src/main.java
@@ -44,26 +44,21 @@ public class main {
     }
 
     public static String createHexFromFields(Integer op, Integer rs, Integer rt, Integer rd, Integer shamt, Integer funct){
-
+// 10 0000 0000 1010 0000 0000 0000 0010
+        // convert all field values binary
+        // sätt ihop alla fields i en sträng
+        // plocka ut 4 bit i taget och omvandla till hexadecimal.
+        // sätt ihop alla
         String initialConcatedString = getHex(op) + getHex(rs) + getHex(rt) + getHex(rd) + getHex(shamt) + getHex(funct);
+        System.out.println("initialConcatedString: " + initialConcatedString);
         Integer shiftToAdd = 0;
 
-
-        // Adding shift to make HEX-string 8 char long
-        if(initialConcatedString.length() < 8){
-            shiftToAdd = 8 - initialConcatedString.length();
-        }
-        String shiftZeros = "";
-        for(int i = 0; i<shiftToAdd; i++){
-            shiftZeros = shiftZeros + "0";
-        }
 
         // Printing pretty
         System.out.println("opcode" + " " + "rs" + " " + "rt" + " " + "rd" + " " + "rd" + " " + "funct");
         System.out.println(getBinary(op) + " " + getBinary(rs) + " " + getBinary(rt) + " " + getBinary(rd) + " " + getBinary(shamt) + " " + getBinary(funct));
 
-
-        String completedHexString = "0x" + getHex(op) + getHex(rs) + getHex(rt) + getHex(rd) + getHex(shamt) + shiftZeros + getHex(funct);
+        String completedHexString = "0x" + getHex(op) + getHex(rs) + getHex(rt) + getHex(rd) + getHex(shamt) + getHex(funct);
         System.out.println(completedHexString);
 
         return completedHexString;
