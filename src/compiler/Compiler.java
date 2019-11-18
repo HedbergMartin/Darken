b6757363611593670527eb08fb4e7680cffd9efd
Martin Hedberg <96mah002@gmail.com>
2019-11-18 13:24:38 +0100

    Added first regex implementation
    Needs to be worked on

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -1,7 +1,12 @@
 package compiler;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Compiler {
 	
@@ -13,11 +18,57 @@ public class Compiler {
 	 * 
 	 * Input: File
 	 */
-	public Compiler() {
-		// TODO Auto-generated constructor stub
+	public Compiler(String pathName) {
+		File file = new File(pathName);
+		try {
+			parseFile(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	private void parseFile() {
+	// R-Command 
+	private void parseFile(File file) throws FileNotFoundException {
+		Scanner sc = new Scanner(file);
+		
+		while (sc.hasNextLine()) {
+			String line = sc.nextLine();
+			System.out.println(line);
+			if (line.isEmpty()) {
+				//save to EmptyCommand without comment
+			} else if (line.charAt(0) == '#') {
+				//save to EmptyCommand with comment
+			} else {
+				String pattern = "([\\S\\-]+:)|([\\w\\-]+)|(\\$[\\w\\-]+)|(#.*)";
+				Pattern r = Pattern.compile(pattern);
+				Matcher m = r.matcher(line);
+				int i = 1;
+				//System.out.println("Group count: " + m.groupCount());
+				while (m.find()) {
+					String s = null;
+					if (i == 1) {
+						s = m.group(1);
+						if (s == null) {
+							i++;
+						}
+					}
+					if (i == 2) {
+						s = m.group(2);
+						if (s == null) {
+							i++;
+						}
+					}
+					if (i == 3) {
+						s = m.group(3);
+						if (s == null) {
+							i++;
+						}
+					}
+					System.out.println("Found: (" + i + ") " + s);
+//					i++;
+				}
+			}
+		}
 		// Read file
 		// Save to arraylist of command
 		
---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import compiler.Compiler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Compiler comp = new Compiler("file.txt");
+	}
+}
