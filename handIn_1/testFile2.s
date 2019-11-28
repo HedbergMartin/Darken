467c02dac251b2127e6d8098b2ef2be404510f77
Emil Söderlind <soderlindemil@gmail.com>
2019-11-28 14:46:40 +0100

    adding jar + src.zip

---
+++ handIn_1/testFile2.s
@@ -0,0 +1,14 @@
+# Test program: j, jr
+
+    addi    $t0, $zero, 64
+    addi    $t1, $zero, 20
+    or      $s0, $t0, $t1
+    jr      $t1
+    nop
+
+    sub     $s1, $t1, $s0
+    nop
+    beq     $s2, $s4, jump_here
+    nop
+    add     $s2, $t0, $s0
+    j       jump_here
