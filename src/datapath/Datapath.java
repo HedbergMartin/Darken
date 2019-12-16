5bdfcdbdc65a64cf3f943de8bc3b9ce5d52d267e
Emil Söderlind <soderlindemil@gmail.com>
2019-12-16 10:52:37 +0100

    Adding datapath + controller classes

---
+++ src/datapath/Datapath.java
@@ -0,0 +1,13 @@
+package datapath;
+
+// (Gränsyta mot CONTROLLERN till GUI ish)
+
+public class Datapath {
+
+    public Datapath() {
+
+        // Skapa alla komponenter
+        // Koppla ihop komponenter
+
+    }
+}
