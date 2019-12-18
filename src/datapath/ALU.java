c4a55c61694d168e51cc356233a3c85441574f8c
Emil Söderlind <soderlindemil@gmail.com>
2019-12-18 13:52:48 +0100

    Implemented ALU

--- src/datapath/ALU.java
+++ src/datapath/ALU.java
@@ -6,6 +6,62 @@ public class ALU {
 
     public void perform(int leftOperand, int rightOperand, int operation){
 
+        switch(operation) {
+            case 0:
+
+                // AND
+                if(leftOperand == 1 && rightOperand == 1){
+                    ALUresult = 1;
+                }else{
+                    ALUresult = 0;
+                }
+
+                break;
+            case 1:
+                // OR
+
+                if(leftOperand == 1 || rightOperand == 1){
+                    ALUresult = 1;
+                }else{
+                    ALUresult = 0;
+                }
+
+                break;
+            case 2:
+                // add
+
+                ALUresult = leftOperand + rightOperand;
+
+                break;
+            case 3:
+                // subtract
+
+                ALUresult = leftOperand - rightOperand;
+
+                break;
+            case 4:
+                // set on less than
+
+                if(leftOperand < rightOperand){
+                    ALUresult = 1;
+                }else{
+                    ALUresult = 0;
+                }
+
+                break;
+            case 5:
+                // NOR
+
+                if(leftOperand != 1 && rightOperand != 1){
+                    ALUresult = 1;
+                }else{
+                    ALUresult = 0;
+                }
+
+                break;
+            default:
+                System.out.println("ALU got invalid operation: " + operation);
+        }
 
 
     }
