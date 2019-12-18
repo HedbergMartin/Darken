0ec441f73ab293e71887e6ab1a83f7cea5dcc272
Emil Söderlind <soderlindemil@gmail.com>
2019-12-18 15:30:39 +0100

    Created a (hopefully) fully wurking ALU control

--- src/datapath/ALUControl.java
+++ src/datapath/ALUControl.java
@@ -2,18 +2,55 @@ package datapath;
 
 public class ALUControl {
 
-    int ALUOp;
+    private int ALUOpOut;
 
-    public void perform(int instruction, int ALUOp){
+    public void perform(int instruction, int ALUOpIn){
 
+        // Page 260 in textbook
+        switch(ALUOpIn) {
+            case 0: // LW/SW
 
+                if(instruction == 43 || instruction == 35){ // lw/sw instruction opcode
+                    ALUOpOut = 2;
+                }
 
+                break;
+            case 1: // Branch equal
+                ALUOpOut = 6;
+
+                break;
+            case 2:
+
+                switch(instruction) { // R-type funct field
+                    case 32: // add
+                        ALUOpOut = 2;
+                        break;
+                    case 34: // subtract
+                        ALUOpOut = 6;
+                        break;
+                    case 36: // AND
+                        ALUOpOut = 0;
+                        break;
+                    case 37: // or
+                        ALUOpOut = 1;
+                        break;
+                    case 42: // set on less than
+                        ALUOpOut = 7;
+                        break;
+                    default:
+                        System.out.println("ALU got invalid instruction: " + instruction);
+                }
+
+                break;
+            default:
+                System.out.println("ALU got invalid ALUOpIn: " + ALUOpIn);
+        }
 
     }
 
 
     public int getALUOp(){
-        return ALUOp;
+        return ALUOpOut;
     }
 
 }
